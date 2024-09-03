8941844ef9f650a18e98593d454b851e5685e003
andrejbarysnikov <andrey1andrey@narod.ru>
2024-09-03 21:52:02 +0300

    3-21

--- Kafka.ConsumerAsync/Program.cs
+++ Kafka.ConsumerAsync/Program.cs
@@ -10,35 +10,57 @@ var channel = Channel.CreateBounded<ConsumeResult<long, OrderEvent>>(
     new BoundedChannelOptions(channelCapasity)
     {
         SingleWriter = true,
-        SingleReader = false,
+        SingleReader = true,
         AllowSynchronousContinuations = true,
+        FullMode = BoundedChannelFullMode.Wait,
     });
-var consumeTask = Task.Factory.StartNew(() => { }, TaskCreationOptions.LongRunning);
-var handleTask = Task.Factory.StartNew(() => { }, TaskCreationOptions.LongRunning);
+
+using var kafkaConsumer = new ConsumerBuilder<long, OrderEvent>(
+        new ConsumerConfig
+        {
+            BootstrapServers = broker,
+            GroupId = "user-notification-service-order-events-listener",
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+            EnableAutoCommit = true,
+            EnableAutoOffsetStore = false,
+            //MaxPollIntervalMs = 5000,
+        })
+    .SetValueDeserializer(new JsonValueSerializer<OrderEvent>())
+    .Build();
+
+var consumeTask = Task.Factory.StartNew(() => Consume(channel.Writer, kafkaConsumer), 
+    TaskCreationOptions.LongRunning).Unwrap();
+var handleTask = Task.Factory.StartNew(() => Handle(channel.Reader, kafkaConsumer), 
+    TaskCreationOptions.LongRunning).Unwrap();
 await Task.WhenAll(consumeTask, handleTask);
 
-async Task Consume(ChannelWriter<ConsumeResult<long, OrderEvent>> channelWriter)
+async Task Handle(ChannelReader<ConsumeResult<long, OrderEvent>> channelReader,
+    IConsumer<long, OrderEvent> consumer)
 {
-    using var consumer = new ConsumerBuilder<long, OrderEvent>(
-            new ConsumerConfig
-            {
-                BootstrapServers = broker,
-                GroupId = "user-notification-service-order-events-listener",
-                AutoOffsetReset = AutoOffsetReset.Earliest,
-                EnableAutoCommit = true,
-                EnableAutoOffsetStore = false,
-                //MaxPollIntervalMs = 5000,
-            })
-        .SetValueDeserializer(new JsonValueSerializer<OrderEvent>())
-        .Build();
-
-    consumer.Subscribe(topic);
-    while (await channelWriter.WaitToWriteAsync())
+    await foreach (var consumerResult in channelReader.ReadAllAsync())
     {
-        while (consumer.Consume() is { } result)
-        {
-            if (channelWriter.TryWrite(result))
-                await channelWriter.WriteAsync(result);
-        }
+        await Task.Delay(100);
+        Console.WriteLine(
+            "{0}:{1}:Handle:{2}:{3}",
+            consumerResult.Partition.Value,
+            consumerResult.Offset.Value,
+            consumerResult.Message.Key,
+            consumerResult.Message.Value);
+        consumer.StoreOffset(consumerResult);
     }
+}
+
+async Task Consume(ChannelWriter<ConsumeResult<long, OrderEvent>> channelWriter,
+    IConsumer<long, OrderEvent> consumer)
+{
+    consumer.Subscribe(topic);
+    while (consumer.Consume() is { } result)
+    {
+        await channelWriter.WriteAsync(result);
+        Console.WriteLine("{0}:{1}:WriteToChannel",
+            result.Partition.Value,
+            result.Offset.Value);
+    }
+    
+    channelWriter.Complete();
 }
